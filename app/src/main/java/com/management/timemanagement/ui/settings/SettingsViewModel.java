35e0cca1e034e6f0b25286ed9a8ab10c17f4057e
Egor <gyvacha@gmail.com>
2020-09-13 16:55:53 +0300

    Menu added

---
+++ app/src/main/java/com/management/timemanagement/ui/settings/SettingsViewModel.java
@@ -0,0 +1,16 @@
+package com.management.timemanagement.ui.settings;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public SettingsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Settings");
+    }
+
+    public LiveData<String> getText() { return mText; }
+}
---
+++ app/src/main/java/com/management/timemanagement/ui/to_do/To_DoViewModel.java
@@ -0,0 +1,16 @@
+package com.management.timemanagement.ui.to_do;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class To_DoViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public To_DoViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("To do list");
+    }
+
+    public LiveData<String> getText() { return mText; }
+}
