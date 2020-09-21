6cd763598dcab0ded8fb7a3509e5b55cb9a0d834
vessau <veronikaspb2017@mail.ru>
2020-09-21 17:08:16 +0300

    DB Adapter test add.

--- app/src/androidTest/java/com/management/timemanagement/data/local/DBAdapterTest.java
+++ app/src/androidTest/java/com/management/timemanagement/data/local/DBAdapterTest.java
@@ -1,37 +1,20 @@
 package com.management.timemanagement.data.local;
 
-import android.content.Context;
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
 
-import androidx.test.espresso.internal.inject.InstrumentationContext;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.management.timemanagement.ui.to_do.To_DoFragment;
-import com.management.timemanagement.ui.to_do.To_DoViewModel;
-
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DBAdapterTest {
-
     DBAdapter adapter = new DBAdapter(InstrumentationRegistry.getInstrumentation().getTargetContext());
-
     @Test
     public void openDB() {
         Assert.assertNotNull(adapter.openDB());
-        adapter.closeDB();
-    }
-
-    @Test
-    public void add() {
-        adapter.openDB();
-        adapter.add("task ", "desc 1", 0);
-        Assert.assertNotNull(adapter.db.query("Tasks", new String[] {"task"},
-                "task=?", new String[] {"task 1"},
-                null, null, null));
         adapter.closeDB();
     }
 
@@ -41,4 +24,24 @@ public class DBAdapterTest {
         Assert.assertNotNull(adapter.getTaskDetailsNotReady());
         adapter.closeDB();
     }
+    @Test
+    public void add() {
+        adapter.openDB();
+        adapter.add("test_task", "test_desc", 0);
+        Cursor cursor = adapter.db.query("Tasks", new String[]{"_id"},
+                "task=?", new String[] {"test_task"},
+                null, null, null);
+        cursor.moveToNext();
+        int id = cursor.getInt(0);
+        Assert.assertNotNull(cursor);
+        adapter.delete(id);
+        adapter.closeDB();
+    }
+    @Test
+    public void delete() {
+        adapter.openDB();
+        Assert.assertNotEquals(adapter.delete(1234), 0);
+        adapter.closeDB();
+    }
+
 }
